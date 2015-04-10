1dfbb9835d7cb3d357fd8344acfe2978264f8b4e
wangxiaoying <wangxiaoying0369@gmail.com>
2015-04-10 16:40:03 +0800

    admin add

--- farmmonitor/farm/admin.py
+++ farmmonitor/farm/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from farm.models import *
 # Register your models here.
+
+class NetworkTestAdmin(admin.ModelAdmin):
+	list_display = ('id', 'message', 'photo')
+
+admin.site.register(NetworkTest, NetworkTestAdmin)
